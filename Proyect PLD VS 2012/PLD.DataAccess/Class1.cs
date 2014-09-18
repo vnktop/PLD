d7f1b0c951f04fd3eb3b874876a01b6ede89c9eb
vnktop <vpinedav23@gmail.com>
2014-09-18 10:16:41 -0500

    Carga de Proyecto PLD
    Esqueleto 5 capas

---
+++ Proyect PLD VS 2012/PLD.DataAccess/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PLD.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ Proyect PLD VS 2012/PLD.Logic/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PLD.Logic
+{
+    public class Class1
+    {
+    }
+}
